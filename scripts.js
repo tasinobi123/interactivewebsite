98da36c5121652e2c7aa0fbd7e666c59c81f9f30
tasinobi123 <tasin.obi123@gmail.com>
2024-12-01 22:42:24 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Quiz Function
+function startQuiz() {
+    alert("Quiz Coming Soon! Prepare your answers! 😊");
+}
+
+// Puzzle Function
+function playPuzzle() {
+    alert("Puzzle feature is under construction. Stay tuned! 🧩");
+}
